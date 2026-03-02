933bf0b3c960df9fa32172a345a7d1a176cffdfd
Ranjith Kumar <ranjith20062007@gmail.com>
2026-03-02 18:57:16 +0530

    First commit

---
+++ ex1.py
@@ -0,0 +1,11 @@
+
+print("="*40)
+print("     TATA CONSULTANCY SERVICES")
+print("~"*40)
+print(" I. RANJITH KUMAR")  
+print(" software developer")
+print(" 6379616413")
+print(" ranjith20062007@gmail.com")
+print("="*40)
+    
+
